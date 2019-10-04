2371b50360cbb57a1a5861100e3ce5425fb1c7e9
Bruno Farina <bfarina@showcasepro.com.br>
2019-10-04 19:23:37 -0300

    Second day - front end

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-a8pps.mongodb.net/semana09?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -9,7 +11,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-a8pps.mongodb.net/s
 const routes = require('./routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
